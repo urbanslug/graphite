1a5d1404cec3387f14aafbd359eb2577642d9de1
Njagi Mwaniki <njagi@urbanslug.com>
2019-06-18 16:02:38 +0300

    Clean up VCF parser to return a list of variations
    A varition is a struct of pos and base/sequence. We want to give back a
    list of these from the parser
    
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- IO/vcf.rkt
+++ IO/vcf.rkt
@@ -1,34 +1,47 @@
 #lang racket
 
-(provide read-vcf save-line variation-hash)
+(provide read-vcf)
 
 ;; store reference file section
 ;; Get spec from https://samtools.github.io/hts-specs/VCFv4.2.pdf
 
-(define variation-hash (make-hash))
+(define variation-file-path
+  "../data/RSV/refererence_and_vcf_file/H_3801_22_04.freebayes.vcf")
+
+;; append to this global variations variable
+(define variations empty)
+
+;; TODO: kmer shouldn't be one char/base but a list of all possibilities there
+(struct variation (position kmer)
+  #:methods gen:custom-write
+  [(define (write-proc variation port mode)
+     (let* ([f (number->string (variation-position variation))]
+           [s (variation-kmer variation)]
+           [p (if (char? s) (string s) s)])
+       (fprintf port (string-append f ": " p))))])
 
 (define (save-line line)
-  ;; Make a hash of pos and base
   (let* ([position-pair (regexp-match-positions #px"[ATCG]\\s" line)]
-         [base-position  (cdr (car position-pair))] ;; sometimes this isn't a position pair but #f
+         ;; sometimes this isn't a position pair but #f
+         [base-position  (cdr (car position-pair))]
          [base (string-ref line base-position)]
          [p (regexp-match-positions #px"\\s\\d*\\s" line)]
          [f (+ (car (car p)) 1)]
          [s (- (cdr (car p)) 1)]
          [position  (string->number (substring line f s))])
-    (hash-set! variation-hash position base)))
+    (set! variations
+          (append variations (list (variation position base))))))
 
 (define (parse-vcf line)
   (when (regexp-match-positions #rx"AF=." line)
-      (let* ([position-pair (regexp-match-positions #rx"AF=." line)]
-             [position  (- (cdr (car position-pair)) 1)]
-             [allele-frequency-char (string-ref line position)]
-             [allele-frequency-num (string->number (make-string 1 allele-frequency-char))]
-             )
-        (when (> allele-frequency-num 0)
-            (save-line line)))))
+    (let* ([position-pair (regexp-match-positions #rx"AF=." line)]
+           [position  (- (cdr (car position-pair)) 1)]
+           [allele-frequency-char (string-ref line position)]
+           [allele-frequency-num (string->number
+                                  (string allele-frequency-char))])
+      (when (> allele-frequency-num 0)
+        (save-line line)))))
 
-;; Read files and display them to the user
 (define (next-line-it file)
   (let ([line (read-line file 'any)])
     (unless (eof-object? line)
@@ -38,12 +51,4 @@
 (define (read-vcf filepath)
   (let* ([port (open-input-file filepath)])
     (next-line-it port)
-    variation-hash))
-
-(struct variation (position kmer) #:transparent)
-
-(define (pair->variation p)
-  (variation (car p) (cdr p)))
-(define (gen-variation-list var-hash)
-  (let ([hash-list (hash->list var-hash)])
-    (sort hash-list (lambda (x y) (< (car x) (car y))))))
+    variations))
