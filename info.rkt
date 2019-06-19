738daedfed25160ad42abc8a062e22cc2dbd9d46
Njagi Mwaniki <njagi@urbanslug.com>
2019-06-20 00:10:11 +0300

    Remove test-include-paths from info.rkt
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- info.rkt
+++ info.rkt
@@ -3,8 +3,5 @@
 (define deps '(("racket" "7.2") "graph"))
 (define version "0.0.0-alpha.1")
 
-;; test only that which is in tests
-(define test-include-paths '("tests"))
-
 ;; ignore whatever is in graphite and this file as well
 (define test-omit-paths '("graphite" "info.rkt"))
