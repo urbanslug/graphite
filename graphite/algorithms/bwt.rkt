ca82228e7f4f89fcffacaf2a3e3cda90bdb40612
Njagi Mwaniki <njagi@urbanslug.com>
2019-06-28 17:00:30 +0300

    Move some of the BWT code into utils
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- graphite/algorithms/bwt.rkt
+++ graphite/algorithms/bwt.rkt
@@ -2,48 +2,22 @@
 
 (require "./utils.rkt")
 
-
-(provide rotate-string
-         unsorted-bwm
-         sort-bwm
-         burrows-wheeler-matrix
+(provide sorted-bwm
          bwt)
 
-;; Burrows Wheeler Matrix (BWM)
-;;;; Burrows Wheeler Transform (BWT)
 
-;; move the first character to the end of the string
-(define/contract (rotate-string str)
-  (string? . -> . string?)
-  (let ([head (string-ref str 0)]
-        [tail (substring str 1)])
-    (~a tail head)))
+(define (sorted-bwm s)
+  (sort-bwm (gen-bwm s)))
 
-(define/contract (unsorted-bwm s [bwt-list empty])
-  (-> string? list?)
-  (if (char=? (string-ref s 0) #\$)
-      bwt-list
-      (if (empty? bwt-list)
-          (let ([dollar-s (~a s "$")])
-            (unsorted-bwm dollar-s (list dollar-s)))
-          (let* ([s* (rotate-string s)]
-                 [bwt-list* (append bwt-list ( list s*))])
-            (unsorted-bwm s* bwt-list*)))))
-
-(define/contract (sort-bwm bwt-list)
-  (list? . -> . list?)
-  (sort bwt-list string<=?))
-
-(define (burrows-wheeler-matrix s)
-  (sort-bwm (unsorted-bwm s)))
-
-(define (sa-string l)
-  (list->string (map (lambda (s*) (string-ref s* 0) ) l)))
 
 (define (bwm-string l)
   (list->string
    (map (lambda (s*) (string-ref s* (- (string-length s*) 1))) l)))
 
+;; ignore spaces for now
+
+;; BWT brings like characters together in runs because it sorts by right context
+
 (define/contract (bwt s)
   (string? . -> . string?)
-  (bwm-string (sort-bwm (unsorted-bwm s))))
+  (bwm-string (sorted-bwm s)))
--- tests/algorithms/test-bwt.rkt
+++ tests/algorithms/test-bwt.rkt
@@ -6,29 +6,8 @@
 
 (define s "abaaba")
 
-(check-equal? (rotate-string s) "baabaa")
-
-(check-equal? (unsorted-bwm s)
-              '("abaaba$"
-                "baaba$a"
-                "aaba$ab"
-                "aba$aba"
-                "ba$abaa"
-                "a$abaab"
-                "$abaaba"))
-
-(check-equal? ((sort-bwm . .> . unsorted-bwm) s)
-              '("$abaaba"
-                "a$abaab"
-                "aaba$ab"
-                "aba$aba"
-                "abaaba$"
-                "ba$abaa"
-                "baaba$a"))
-
-(check-equal? (burrows-wheeler-matrix s)
-              ((sort-bwm . .> . unsorted-bwm) s))
-
+(check-equal? (sorted-bwm s)
+              (sort (gen-bwm s) string<=?))
 
 (check-equal? (bwt s)
               "abba$aa")
