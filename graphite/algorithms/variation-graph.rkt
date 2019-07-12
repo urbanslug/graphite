4f98d086198a346879d2d853852d7e1806b16958
Njagi Mwaniki <njagi@urbanslug.com>
2019-07-12 18:51:07 +0300

    Generate proper variation graph without duplicates in the variation
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- graphite/algorithms/variation-graph.rkt
+++ graphite/algorithms/variation-graph.rkt
@@ -20,54 +20,61 @@
   (if (empty? vcf)
       ;; base case
       (map
-       (lambda (prev-node)
-         (cons (list->string (take ref prev-pos)) prev-node)) (car prev-nodes))
+       (lambda (prev-node) (cons (create-node (list->string (take ref prev-pos)) #:offset 0)
+                                 prev-node))
+       (car prev-nodes))
 
-      ;; recursive call
+      ;; Recursive call
       (let* ([current (last vcf)]
-             [pos (car current)]
-             [seg-var (~a (cdr current))]
+             ;; because the VCF is 1 indexed
+             [pos (- (variation-position current) 1)]
+             [seg-var (~a (variation-kmer current))]
 
              [seg-ref* (if (and (number? prev-pos) (= pos prev-pos))
-                  (cdr prev-nodes)
-                  (slice ref (+ 1 pos) prev-pos))]
-             [seg-ref (if (string? seg-ref*)
-                          seg-ref*
-                          (list->string seg-ref*))]
+                           (cdr prev-nodes)
+                           (slice ref (+ 1 pos) prev-pos))]
+
+             ;; How did this happen?
+             ;; this is bad we should be using nodes here
+             [seg-ref (cond [(string? seg-ref*) seg-ref*]
+                            [(list? seg-ref*) (list->string seg-ref*)]
+                            ;; assume that it's a node or  a string
+                            [else seg-ref*
+                                  ])]
 
              [orig-var (~a
                         (if (and (number? prev-pos) (= pos prev-pos))
                             (first (car prev-nodes))
                             (list-ref ref pos))) ]
 
-             ;[alt-node (create-node seg-var #offset pos)]
-             ;[adj (create-node seg-ref #offset (+ 1 pos))]
-             ;[orig-node (create-node orig-var #offset pos)]
-             )
-
+             [alt-node (create-node seg-var #:offset pos)]
+             [tail-node (if (string? seg-ref)
+                            (create-node seg-ref #:offset (+ 1 pos))
+                            seg-ref)]
+             [orig-node (create-node orig-var #:offset pos)])
 
         (append empty
                 ;; connect current string ref to previous nodes
-                (map (lambda (prev-node)  (cons seg-ref prev-node))
+                (map (lambda (prev-node)  (cons tail-node prev-node))
                      (if (and (number? prev-pos) (not (= pos prev-pos)) (pair? prev-nodes))
                          (car prev-nodes)
                          empty))
 
                 ;; connect variation and original to "tail"
-
-                (list (cons seg-var seg-ref) (cons orig-var seg-ref))
+                (list (cons alt-node tail-node) (cons orig-node tail-node))
 
                 ;; call with rest
                 (gen-nodes ref
                           (drop-right vcf 1)
                           pos
                           (cons (if (and (number? prev-pos) (= pos prev-pos))
-                                    (remove-duplicates (append (list orig-var seg-var) (car prev-nodes)))
-                                    (remove-duplicates (list orig-var seg-var)))
-                                seg-ref))))))
+                                    (remove-duplicates (append (list orig-node alt-node) (car prev-nodes)))
+                                    (remove-duplicates (list orig-node alt-node)))
+                                tail-node))))))
 
 
 ;; Generate a graph from a reference and VCF
 (define (gen-vg reference variation-list)
-  (gen-directed-graph
+  (gen-directed-graph empty-graph
    (gen-nodes (string->list reference) variation-list)))
+
