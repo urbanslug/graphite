0372d62e332d74eb0105aef679f99e14c06fca56
Njagi Mwaniki <njagi@urbanslug.com>
2019-07-12 13:46:32 +0300

    Start gen-vg that handles alts in variation
    We want to be able to have multiple alternatives at one position in the
    same VCF file still work with `gen-vg`
    
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- graphite/algorithms/variation-graph.rkt
+++ graphite/algorithms/variation-graph.rkt
@@ -8,54 +8,66 @@
 (require "../structures/graph.rkt")
 (require "../structures/variations.rkt")
 
+;; Force everything into strings for now at least
+;; May have some duplicates but it won't matter
+(define (gen-nodes ref
+                  vcf
+                  [prev-pos #f]
+                  ;; a pair of prev-nodes and prev tail
+                  ;; prev-nodes should be a set  to avoid duplicates
+                  [prev-nodes empty])
+
+  (if (empty? vcf)
+      ;; base case
+      (map
+       (lambda (prev-node)
+         (cons (list->string (take ref prev-pos)) prev-node)) (car prev-nodes))
+
+      ;; recursive call
+      (let* ([current (last vcf)]
+             [pos (car current)]
+             [seg-var (~a (cdr current))]
+
+             [seg-ref* (if (and (number? prev-pos) (= pos prev-pos))
+                  (cdr prev-nodes)
+                  (slice ref (+ 1 pos) prev-pos))]
+             [seg-ref (if (string? seg-ref*)
+                          seg-ref*
+                          (list->string seg-ref*))]
+
+             [orig-var (~a
+                        (if (and (number? prev-pos) (= pos prev-pos))
+                            (first (car prev-nodes))
+                            (list-ref ref pos))) ]
+
+             ;[alt-node (create-node seg-var #offset pos)]
+             ;[adj (create-node seg-ref #offset (+ 1 pos))]
+             ;[orig-node (create-node orig-var #offset pos)]
+             )
+
+
+        (append empty
+                ;; connect current string ref to previous nodes
+                (map (lambda (prev-node)  (cons seg-ref prev-node))
+                     (if (and (number? prev-pos) (not (= pos prev-pos)) (pair? prev-nodes))
+                         (car prev-nodes)
+                         empty))
+
+                ;; connect variation and original to "tail"
+
+                (list (cons seg-var seg-ref) (cons orig-var seg-ref))
+
+                ;; call with rest
+                (gen-nodes ref
+                          (drop-right vcf 1)
+                          pos
+                          (cons (if (and (number? prev-pos) (= pos prev-pos))
+                                    (remove-duplicates (append (list orig-var seg-var) (car prev-nodes)))
+                                    (remove-duplicates (list orig-var seg-var)))
+                                seg-ref))))))
+
 
 ;; Generate a graph from a reference and VCF
-;; list -> list -> hash map
-(define (gen-vg-uncapped ref vcf [g empty-graph])
-  (foldr
-   (lambda (variation* accum)
-     (let* ([g*             (hash-ref accum 'graph)]
-            [prev-split-pos (hash-ref accum 'prev-split-pos)]
-            [prev-nodes     (hash-ref accum 'prev-nodes)]
-
-            [kmer*    (variation-kmer variation*)]
-            [pos      (variation-position variation*)]
-            [next-pos (+ pos 1)]
-
-            [fragment (slice ref pos (if prev-split-pos prev-split-pos #f))]
-
-            ;; fragment
-            [n1 (create-node (list->string fragment) #:offset next-pos)]
-            ;; seq at point
-            [n2 (create-node (string (list-ref ref pos)) #:offset next-pos)]
-            ;; variation
-            [n3 (create-node kmer* #:offset next-pos)]
-
-            [x (if (empty? prev-nodes)
-                   empty
-                   (map (lambda (x) (cons n1 x)) prev-nodes))]
-
-            [nodes (append (list (cons n2 n1) (cons n3 n1)) x)]
-            [y (gen-directed-graph g* nodes)])
-
-       (hash
-        'graph y
-        'prev-nodes (list (hash-ref y (node-id n2))
-                          (hash-ref y (node-id n3)))
-        'prev-split-pos pos)))
-
-   (hash 'graph g
-         'prev-nodes empty
-         ;; where we last split the ref
-         'prev-split-pos #f)
-   vcf))
-
-(define (gen-vg ref vcf [g empty-graph])
-  (let* ([uncapped (gen-vg-uncapped (string->list ref) vcf g)]
-         [cap-frag (slice (string->list ref) 0 (variation-position (first vcf)))]
-         [cap-node (create-node (list->string  cap-frag) #:offset 0)])
-
-    (gen-directed-graph
-     (hash-ref uncapped 'graph)
-     (list (cons cap-node (first (hash-ref uncapped 'prev-nodes)))
-           (cons cap-node (second (hash-ref uncapped 'prev-nodes)))))))
+(define (gen-vg reference variation-list)
+  (gen-directed-graph
+   (gen-nodes (string->list reference) variation-list)))
