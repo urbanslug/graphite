b714265e82a8f4babee49c83625603cf8528324e
Njagi Mwaniki <njagi@urbanslug.com>
2019-08-06 12:52:39 +0300

    Remove deps
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- info.rkt
+++ info.rkt
@@ -1,6 +1,5 @@
 #lang info
 (define collection "graphite")
-(define deps '(("racket" "7.3")))
 (define version "0.0.0-alpha.1")
 
 (define scribblings '(("graphite/graphite.scrbl" (multi-page toc))))
