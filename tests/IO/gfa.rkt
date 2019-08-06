bdb6e4ce3691a864bd18af8880899d16bd51f77a
Njagi Mwaniki <njagi@urbanslug.com>
2019-08-06 12:49:05 +0300

    Remove gfa tests
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- tests/IO/gfa.rkt
+++ tests/IO/gfa.rkt
@@ -5,16 +5,3 @@
 (require "../../graphite/IO/gfa.rkt")
 (require "../../graphite/structures/variations.rkt")
 (require "../../graphite/algorithms/variation-graph.rkt")
-
-
-(define reference
-  "ATTTCCGATAGATCGATATGCGATGCGATGCAGTAGC")
-
-(define v1 (variation 10 "TGA"))
-(define v2 (variation 15 "ACA"))
-(define v3 (variation 30 "CCA"))
-(define variations (list v1 v2 v3))
-
-(define vg (gen-vg reference variations))
-
-(write-gfa (vg->gfa-string vg))
